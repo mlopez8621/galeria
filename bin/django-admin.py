6c368393c97f3e8de7241977f4e867a32e92a097
mlopeza1 <mlopeza1@ucentral.edu.co>
2018-09-12 22:44:01 -0500

    Commit inicial

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/michel/PycharmProjects/galeria/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ polls/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns=[
+    url(r'^$', views.index, name='index'),
+    url(r'^add/$', views.add_image, name='addImage'),
+    url(r'^addUser/$', views.add_user_view, name='addUser'),
+    url(r'^login/$', views.login_view, name='login'),
+    url(r'^logout/$', views.logout_view, name='logout'),
+]
